f230774f9284f5b05963511b899c4e7924d04b66
Mark Scherer <dereuromark@users.noreply.github.com>
2022-10-19 04:23:55 +0200

    Fix up hasMany (#6)
    * Fix up hasMany

--- tests/TestCase/Model/Table/ItemsTableTest.php
+++ tests/TestCase/Model/Table/ItemsTableTest.php
@@ -40,6 +40,8 @@ class ItemsTableTest extends FileStorageTestCase
                 'Avatars.model' => 'Items',
             ],
             'joinType' => 'LEFT',
+            'dependent' => true,
+            'cascadeCallbacks' => true,
         ]);
 
         $this->table->hasMany('Photos', [
@@ -49,6 +51,8 @@ class ItemsTableTest extends FileStorageTestCase
                 'Photos.model' => 'Items',
             ],
             'joinType' => 'LEFT',
+            'dependent' => true,
+            'cascadeCallbacks' => true,
         ]);
 
         $this->table->addBehavior(
@@ -83,7 +87,86 @@ class ItemsTableTest extends FileStorageTestCase
     /**
      * @return void
      */
-    public function testUploadNew()
+    public function testUploadHasMany()
+    {
+        // Upload first pic
+        $entity = $this->table->newEntity([
+            'name' => 'Test',
+            'photos' => [
+                [
+                    'file' => new UploadedFile(
+                        $this->fileFixtures . 'titus.jpg',
+                        filesize($this->fileFixtures . 'titus.jpg'),
+                        UPLOAD_ERR_OK,
+                        'tituts.jpg',
+                        'image/jpeg',
+                    ),
+                ],
+            ],
+        ]);
+        $this->assertSame([], $entity->getErrors());
+
+        $this->table->saveOrFail($entity);
+
+        $entity = $this->table->get($entity->id, ['contain' => 'Photos']);
+
+        $this->assertNotEmpty($entity->photos);
+
+        foreach ($entity->photos as $photo) {
+            $this->assertSame('Items', $photo->model);
+            $this->assertNotEmpty($photo->foreign_key);
+            $this->assertSame('Photos', $photo->collection);
+            $this->assertNotEmpty($photo->metadata);
+            $this->assertNotEmpty($photo->variants);
+            $this->assertStringStartsWith('Items/Photos/', $photo->path);
+
+            $expected = [
+                'width' => 512,
+                'height' => 768,
+            ];
+            $this->assertSame($expected, $photo->metadata);
+        }
+
+        // Upload second pic
+        $entity = $this->table->patchEntity($entity, [
+            'photos' => [
+                [
+                    'file' => new UploadedFile(
+                        $this->fileFixtures . 'demo.png',
+                        filesize($this->fileFixtures . 'demo.png'),
+                        UPLOAD_ERR_OK,
+                        'demo.png',
+                        'image/png',
+                    ),
+                ],
+            ],
+        ]);
+        $this->assertSame([], $entity->getErrors());
+
+        $this->table->saveOrFail($entity);
+
+        $entity = $this->table->get($entity->id, ['contain' => 'Photos']);
+        $this->assertCount(2, $entity->photos);
+
+        $photo2 = array_pop($entity->photos);
+        $this->assertSame('Items', $photo2->model);
+        $this->assertNotEmpty($photo2->foreign_key);
+        $this->assertSame('Photos', $photo2->collection);
+        $this->assertNotEmpty($photo2->metadata);
+        $this->assertNotEmpty($photo2->variants);
+        $this->assertStringStartsWith('Items/Photos/', $photo2->path);
+
+        $expected = [
+            'width' => 512,
+            'height' => 512,
+        ];
+        $this->assertSame($expected, $photo2->metadata);
+    }
+
+    /**
+     * @return void
+     */
+    public function testUploadHasOneNew()
     {
         $entity = $this->table->newEntity([
             'name' => 'Test',
@@ -116,7 +199,7 @@ class ItemsTableTest extends FileStorageTestCase
     /**
      * @return void
      */
-    public function testUploadOverwriteExisting()
+    public function testUploadHasOneOverwriteExisting()
     {
         // Upload first pic
         $entity = $this->table->newEntity([
@@ -178,5 +261,39 @@ class ItemsTableTest extends FileStorageTestCase
             'height' => 512, // !!!
         ];
         $this->assertSame($expected, $entity->avatar->metadata);
+    }
+
+    /**
+     * @return void
+     */
+    public function testUploadHasOneDelete()
+    {
+        $entity = $this->table->newEntity([
+            'name' => 'Test',
+            'avatar' => [
+                'file' => new UploadedFile(
+                    $this->fileFixtures . 'titus.jpg',
+                    filesize($this->fileFixtures . 'titus.jpg'),
+                    UPLOAD_ERR_OK,
+                    'tituts.jpg',
+                    'image/jpeg',
+                ),
+            ],
+        ]);
+        $this->assertSame([], $entity->getErrors());
+
+        $this->table->saveOrFail($entity);
+
+        $entity = $this->table->get($entity->id, ['contain' => 'Avatars']);
+
+        $this->assertNotEmpty($entity->avatar);
+        $this->assertNotEmpty($entity->avatar->metadata);
+        $this->assertNotEmpty($entity->avatar->variants);
+
+        $result = $this->table->delete($entity);
+        $this->assertTrue($result);
+
+        $count = $this->table->Avatars->find()->count();
+        $this->assertSame(0, $count);
     }
 }
