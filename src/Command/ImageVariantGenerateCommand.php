8d7a2133c2efc5a3ff58db13329ee44b7380e899
mscherer <mark.scherer@spryker.com>
2022-11-16 01:03:18 +0100

    Add basic (re)generate command

---
+++ src/Command/ImageVariantGenerateCommand.php
@@ -0,0 +1,261 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace FileStorage\Command;
+
+use Cake\Command\Command;
+use Cake\Console\Arguments;
+use Cake\Console\ConsoleIo;
+use Cake\Console\ConsoleOptionParser;
+use Cake\Core\Configure;
+use Cake\ORM\TableRegistry;
+use Exception;
+
+class ImageVariantGenerateCommand extends Command
+{
+    /**
+     * Storage Table Object
+     *
+     * @var \Cake\ORM\Table
+     */
+    protected $Table;
+
+    /**
+     * @var int
+     */
+    protected $limit = 10;
+
+    /**
+     * Implement this method with your command's logic.
+     *
+     * @param \Cake\Console\Arguments $args The command arguments.
+     * @param \Cake\Console\ConsoleIo $io The console io
+     *
+     * @return int|null|void The exit code or null for success
+     */
+    public function execute(Arguments $args, ConsoleIo $io)
+    {
+        $storageTable = (string)$args->getOption('storage');
+        try {
+            $this->Table = TableRegistry::getTableLocator()->get($storageTable);
+        } catch (Exception $e) {
+            $io->abort($e->getMessage());
+        }
+
+        if ($args->getOption('limit')) {
+            $this->limit = (int)$args->getOption('limit');
+        }
+
+        $model = $args->getArgumentAt(0);
+        $collection = $args->getArgumentAt(1);
+        $key = $model;
+        // Hack for now
+        if ($key) {
+            $key = $collection;
+        }
+        if ($collection) {
+            $key .= '.' . $collection;
+        }
+
+        if ($key) {
+            $operations = Configure::read('FileStorage.imageVariants.' . $key);
+        } else {
+            $operations = Configure::read('FileStorage.imageVariants');
+        }
+
+        if (!$operations) {
+            $io->abort(__('Cannot find variants config for this model/collection.'));
+        }
+
+        if ($key) {
+            $operations = [
+                $model => [
+                    $collection => $operations,
+                ],
+            ];
+        }
+
+        return $this->_process($args, $io, $operations);
+    }
+
+    /**
+     * @param \Cake\Console\Arguments $args
+     * @param \Cake\Console\ConsoleIo $io
+     * @param array<string, mixed> $operations
+     *
+     * @return int
+     */
+    protected function _process(Arguments $args, ConsoleIo $io, array $operations): int
+    {
+        foreach ($operations as $model => $collections) {
+            foreach ($collections as $collection => $variants) {
+                $io->out('### ' . $model . ' . ' . $collection);
+                $totalImageCount = $this->_getCount($model, $collection);
+
+                if ($totalImageCount === 0) {
+                    $io->out(__('No images found for this model/collection'));
+
+                    continue;
+                }
+
+                $io->out(__('{0} image file(s) will be processed', $totalImageCount));
+
+                $options = $args->getOptions();
+                $this->_loop($io, $model, $collection, $variants, $options);
+            }
+        }
+
+        return static::CODE_SUCCESS;
+    }
+
+    /**
+     * Loops through image records and performs requested operation on them.
+     *
+     * @param \Cake\Console\ConsoleIo $io
+     * @param string $model
+     * @param string $collection
+     * @param array<string, mixed> $operations
+     * @param array<string, mixed> $options
+     *
+     * @return void
+     */
+    protected function _loop(ConsoleIo $io, string $model, string $collection, array $operations = [], array $options = []): void
+    {
+        $offset = 0;
+        $limit = $this->limit;
+
+        /** @var \Phauthentic\Infrastructure\Storage\FileStorage|null $storage */
+        $storage = Configure::read('FileStorage.behaviorConfig.fileStorage');
+        if (!$storage) {
+            $io->abort(sprintf('Invalid adapter config `%s` provided!', $options['adapter']));
+        }
+        $adapter = $storage->getStorage($options['adapter']);
+
+        do {
+            $images = $this->_getRecords($model, $collection, $limit, $offset);
+            if ($images) {
+                foreach ($images as $image) {
+                    $payload = [
+                        'entity' => $image,
+                        'storage' => $adapter,
+                        'operations' => $operations,
+                        'table' => $this->Table,
+                        'options' => $options,
+                    ];
+
+                    //$Event = new Event('ImageVariant.generate', $this->Table, $payload);
+                    //EventManager::instance()->dispatch($Event);
+
+                    // Generate
+
+                    $io->verbose(__('- ID {0} processed', $image->id));
+                }
+            }
+            $offset += $limit;
+        } while ($images);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getOptionParser(): ConsoleOptionParser
+    {
+        $parser = parent::getOptionParser();
+        $parser->setDescription([
+            __('Command for (re)generating image variants.'),
+        ]);
+        $parser->addOption('storage', [
+            'short' => 's',
+            'help' => __('The storage table for image processing you want to use.'),
+            'default' => 'FileStorage.FileStorage',
+        ]);
+        $parser->addOption('limit', [
+            'short' => 'l',
+            'help' => __('Limits the amount of records to be processed in one batch'),
+        ]);
+        $parser->addOption('dryRun', [
+            'short' => 'd',
+            'help' => __('Dry-Run only.'),
+        ]);
+        $parser->addArguments(
+            [
+                'model' => [
+                    'help' => __('Value of the model property of the images to generate'),
+                    'required' => false,
+                ],
+                'collection' => [
+                    'help' => __('The image collection.'),
+                    'required' => false,
+                ],
+            ],
+        );
+        $parser->addOptions(
+            [
+                'adapter' => [
+                    'short' => 'a',
+                    'help' => __('The adapter config name to use.'),
+                    'default' => 'Local',
+                ],
+                'force' => [
+                    'short' => 'f',
+                    'help' => __('Force overwriting of existing versions (e.g. after a config change).'),
+                    'boolean' => true,
+                ],
+            ],
+        );
+
+        return $parser;
+    }
+
+    /**
+     * Gets the amount of images for a model in the DB.
+     *
+     * @param string $model
+     * @param string $collection
+     * @param array $extensions
+     *
+     * @return int
+     */
+    protected function _getCount(string $model, string $collection, array $extensions = ['jpg', 'png', 'jpeg']): int
+    {
+        $conditions = [
+            'model' => $model,
+            'collection' => $collection,
+            'extension IN' => $extensions,
+        ];
+
+        return $this->Table
+            ->find()
+            ->where($conditions)
+            ->count();
+    }
+
+    /**
+     * Gets the chunk of records for the image processing
+     *
+     * @param string $model
+     * @param string $collection
+     * @param int $limit
+     * @param int $offset
+     * @param array $extensions
+     *
+     * @return array<\FileStorage\Model\Entity\FileStorage>
+     */
+    protected function _getRecords(string $model, string $collection, int $limit, int $offset, array $extensions = ['jpg', 'png', 'jpeg']): array
+    {
+        $conditions = [
+            'model' => $model,
+            'collection' => $collection,
+            'extension IN' => $extensions,
+        ];
+
+        return $this->Table
+            ->find()
+            ->where($conditions)
+            ->limit($limit)
+            ->offset($offset)
+            ->all()
+            ->toArray();
+    }
+}
