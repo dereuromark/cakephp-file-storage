680e78c515a4bef985ffe0f1e8f76b725a588165
mscherer <dereuromark@web.de>
2026-03-25 18:06:36 +0100

    Add isValidImage() validation method
    Allows checking if an uploaded file is a valid image before running
    dimension checks. This enables clearer error messages like "File must
    be a valid image" instead of confusing dimension errors for non-image
    uploads.
    
    Supports optional array of allowed IMAGETYPE_* constants, defaulting
    to JPEG, PNG, GIF, and WebP.

--- src/Model/Validation/ImageValidationTrait.php
+++ src/Model/Validation/ImageValidationTrait.php
@@ -7,6 +7,40 @@ use Psr\Http\Message\UploadedFileInterface;
 trait ImageValidationTrait
 {
     /**
+     * Check if the uploaded file is a valid image.
+     *
+     * Use this validation before dimension checks to provide a clear error
+     * message when non-image files are uploaded.
+     *
+     * @param \Psr\Http\Message\UploadedFileInterface|array $check Value to check
+     * @param array<int> $allowedTypes Allowed image types (IMAGETYPE_* constants). Defaults to JPEG, PNG, GIF, WebP.
+     *
+     * @return bool Success
+     */
+    public static function isValidImage($check, array $allowedTypes = []): bool
+    {
+        if ($check instanceof UploadedFileInterface) {
+            $file = $check->getStream()->getMetadata('uri');
+        } else {
+            if (!isset($check['tmp_name']) || !strlen($check['tmp_name'])) {
+                return false;
+            }
+            $file = $check['tmp_name'];
+        }
+
+        $imageInfo = @getimagesize($file);
+        if ($imageInfo === false) {
+            return false;
+        }
+
+        if (!$allowedTypes) {
+            $allowedTypes = [IMAGETYPE_JPEG, IMAGETYPE_PNG, IMAGETYPE_GIF, IMAGETYPE_WEBP];
+        }
+
+        return in_array($imageInfo[2], $allowedTypes, true);
+    }
+
+    /**
      * Check that the file is above the minimum width requirement
      *
      * @param \Psr\Http\Message\UploadedFileInterface|array $check Value to check
