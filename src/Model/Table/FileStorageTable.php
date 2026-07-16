98b9f2cc732529c2c08133af5c611ccff5e3f9ab
Mark Scherer <dereuromark@users.noreply.github.com>
2026-07-16 21:10:26 +0200

    Adopt TEntity table template + tighten Table method annotations (#34)
    * Adopt TEntity table template + tighten Table method annotations
    
    Re-runs the latest dereuromark/cakephp-ide-helper model annotator on
    FileStorageTable. Adds the second TEntity template parameter to the
    @extends declaration, and tightens patchEntity / save / saveMany /
    loadInto / delete method signatures from generic EntityInterface to
    the concrete FileStorage entity class.
    
    This lets PHPStan flow the entity type end-to-end through
    `find()->first()` and `find()->toArray()` chains in downstream code,
    removing inline @var annotations consumer apps currently sprinkle on
    every callsite.
    
    Floor bump: cakephp/cakephp ^5.3.4 (the release that introduced the
    second TEntity template on Cake\ORM\Table). Older versions would
    surface a `generics.moreTypes` error from PHPStan.
    
    * Reorder docblock tags to satisfy PhpCollective.Commenting.DocBlockTagOrder
    
    * Bump README CakePHP version floor to 5.3.4+

--- src/Model/Table/FileStorageTable.php
+++ src/Model/Table/FileStorageTable.php
@@ -23,20 +23,23 @@ use FileStorage\Model\Entity\FileStorage;
  * @author Florian Krämer
  * @copyright 2012 - 2020 Florian Krämer
  * @license MIT
- *
+ * @extends \Cake\ORM\Table<array{FileStorage: \FileStorage\Model\Behavior\FileStorageBehavior, Timestamp: \Cake\ORM\Behavior\TimestampBehavior}, \FileStorage\Model\Entity\FileStorage>
  * @method \FileStorage\Model\Entity\FileStorage newEmptyEntity()
  * @method \FileStorage\Model\Entity\FileStorage newEntity(array $data, array $options = [])
  * @method array<\FileStorage\Model\Entity\FileStorage> newEntities(array $data, array $options = [])
  * @method \FileStorage\Model\Entity\FileStorage get(mixed $primaryKey, array|string $finder = 'all', \Psr\SimpleCache\CacheInterface|string|null $cache = null, \Closure|string|null $cacheKey = null, mixed ...$args)
- * @method \FileStorage\Model\Entity\FileStorage findOrCreate($search, ?callable $callback = null, array $options = [])
- * @method \FileStorage\Model\Entity\FileStorage patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
- * @method array<\FileStorage\Model\Entity\FileStorage> patchEntities(iterable $entities, array $data, array $options = [])
- * @method \FileStorage\Model\Entity\FileStorage|false save(\Cake\Datasource\EntityInterface $entity, array $options = [])
- * @method \FileStorage\Model\Entity\FileStorage saveOrFail(\Cake\Datasource\EntityInterface $entity, array $options = [])
- * @method \Cake\Datasource\ResultSetInterface<\FileStorage\Model\Entity\FileStorage>|false saveMany(iterable $entities, array $options = [])
- * @method \Cake\Datasource\ResultSetInterface<\FileStorage\Model\Entity\FileStorage> saveManyOrFail(iterable $entities, array $options = [])
- * @method \Cake\Datasource\ResultSetInterface<\FileStorage\Model\Entity\FileStorage>|false deleteMany(iterable $entities, array $options = [])
- * @method \Cake\Datasource\ResultSetInterface<\FileStorage\Model\Entity\FileStorage> deleteManyOrFail(iterable $entities, array $options = [])
+ * @method \FileStorage\Model\Entity\FileStorage findOrCreate(\Cake\ORM\Query\SelectQuery|callable|array $search, ?callable $callback = null, array $options = [])
+ * @method \FileStorage\Model\Entity\FileStorage patchEntity(\FileStorage\Model\Entity\FileStorage $entity, array $data, array $options = [])
+ * @method array<\FileStorage\Model\Entity\FileStorage> patchEntities(iterable<\FileStorage\Model\Entity\FileStorage> $entities, array $data, array $options = [])
+ * @method \FileStorage\Model\Entity\FileStorage|false save(\FileStorage\Model\Entity\FileStorage $entity, array $options = [])
+ * @method \FileStorage\Model\Entity\FileStorage saveOrFail(\FileStorage\Model\Entity\FileStorage $entity, array $options = [])
+ * @method \Cake\Datasource\ResultSetInterface<\FileStorage\Model\Entity\FileStorage>|false saveMany(iterable<\FileStorage\Model\Entity\FileStorage> $entities, array $options = [])
+ * @method \Cake\Datasource\ResultSetInterface<\FileStorage\Model\Entity\FileStorage> saveManyOrFail(iterable<\FileStorage\Model\Entity\FileStorage> $entities, array $options = [])
+ * @method \Cake\Datasource\ResultSetInterface<\FileStorage\Model\Entity\FileStorage>|false deleteMany(iterable<\FileStorage\Model\Entity\FileStorage> $entities, array $options = [])
+ * @method \Cake\Datasource\ResultSetInterface<\FileStorage\Model\Entity\FileStorage> deleteManyOrFail(iterable<\FileStorage\Model\Entity\FileStorage> $entities, array $options = [])
+ * @method bool delete(\FileStorage\Model\Entity\FileStorage $entity, array $options = [])
+ * @method bool deleteOrFail(\FileStorage\Model\Entity\FileStorage $entity, array $options = [])
+ * @method \FileStorage\Model\Entity\FileStorage|array<\FileStorage\Model\Entity\FileStorage> loadInto(\FileStorage\Model\Entity\FileStorage|array<\FileStorage\Model\Entity\FileStorage> $entities, array $contain)
  * @mixin \Cake\ORM\Behavior\TimestampBehavior
  * @mixin \FileStorage\Model\Behavior\FileStorageBehavior
  */
